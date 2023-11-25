001b50adedbfbf115ff05c64f1c4432b5ca614ca
Wilber <galvezwilber22@gmail.com>
2023-11-25 19:50:28 -0400

    almost finished

--- app/src/main/java/com/wgg/shopassistant/MainActivity.kt
+++ app/src/main/java/com/wgg/shopassistant/MainActivity.kt
@@ -4,6 +4,7 @@ import AjustesScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -23,8 +24,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,6 +37,7 @@ import com.wgg.shopassistant.ui.navigation.BottomNavigation
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.wgg.shopassistant.ui.listas.AgregarScreen
+import com.wgg.shopassistant.ui.listas.ListaScreen
 import com.wgg.shopassistant.ui.theme.NavigationDrawerSwitchThemeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -45,7 +49,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val isDarkTheme = rememberSaveable { mutableStateOf(false) }
+            val isDarkTheme = rememberSaveable { mutableStateOf( true) }
 
             NavigationDrawerSwitchThemeTheme(darkTheme = isDarkTheme.value) {
 
@@ -106,7 +110,7 @@ class MainActivity : ComponentActivity() {
 
                                 }
                                 composable("lista") {
-                                    Klk()
+                                    ListaScreen()
 
                                 }
                                 composable("planificar") {
---
+++ app/src/main/java/com/wgg/shopassistant/ui/listas/listaScreen.kt
@@ -0,0 +1,169 @@
+package com.wgg.shopassistant.ui.listas
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.wgg.shopassistant.data.local.entities.ListaConDetalles
+import com.wgg.shopassistant.data.local.entities.ListaDetalle
+import dagger.multibindings.ClassKey
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListaScreen(
+    viewModel: listasViewModel = hiltViewModel()
+) {
+    val listas by viewModel.litas.collectAsState()
+
+    Scaffold() {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            items(
+                items = listas.reversed(),
+                itemContent = { listaConDetalles ->
+                    ListaItem(listaConDetalles)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun ListaItem(listaConDetalles: ListaConDetalles) {
+    var detallesVisible by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp)
+            ) {
+                Text(
+                    text = listaConDetalles.lista.fecha,
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    modifier = Modifier.weight(1f)
+                )
+                IconButton(
+                    onClick = { detallesVisible = !detallesVisible },
+                    modifier = Modifier.size(24.dp)
+                ) {
+                    Icon(
+                        imageVector = if (detallesVisible) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                        contentDescription = if (detallesVisible) "Ocultar detalles" else "Mostrar detalles"
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            if (detallesVisible) {
+                listaConDetalles.detalles.forEach { detalle ->
+                    DetalleList(detalle)
+                }
+
+                Spacer(modifier = Modifier.height(25.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp)
+                ) {
+                    Text(
+                        text = "TOTAL:",
+                        modifier = Modifier
+                            .weight(1f)
+                            .wrapContentWidth(Alignment.Start),
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text =listaConDetalles.lista.totalPrecio.toString() ,
+                        modifier = Modifier
+                            .weight(1f)
+                            .wrapContentWidth(Alignment.End)
+                    )
+                }
+
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun DetalleList(listaDetalle: ListaDetalle){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+    ) {
+        Text(
+            text = listaDetalle.nombre +" X " +listaDetalle.cantidad.toString(),
+            modifier = Modifier
+                .weight(1f)
+                .wrapContentWidth(Alignment.Start),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = listaDetalle.total.toString(),
+            modifier = Modifier
+                .weight(1f)
+                .wrapContentWidth(Alignment.End)
+        )
+    }
+
+}
+
+
+
